ddcf9bf0778d45986d167172a9536676bf2c59a2
sarafat-hossen <hshowrab@gmail.com>
2021-09-14 08:43:26 +0600

    html added

---
+++ custom.js
@@ -0,0 +1,11 @@
+var num1 = 100;
+var num2 = 10;
+if( num1 < 10){
+document.write('<h1>Number1 is bigger than number2</h1>')
+}
+else if(num1 == num2){
+    document.write('<h2>both are equal</h2>')
+}
+else{
+    document.write('<h2>Number 2 is bigger than number 1</h2>')
+}
